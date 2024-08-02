909470ef9f48cb346f22de6c680709b2f3274503
KaterynaNat <kate.naturkach@gmail.com>
2024-08-02 21:21:24 +0300

    Update mob-menu.js

--- js/mob-menu.js
+++ js/mob-menu.js
@@ -1,10 +1,8 @@
 (() => {
   const refs = {
-    // Додати атрибут data-modal-open на кнопку відкриття
+  
     openModalBtn: document.querySelector("[data-burger-open]"),
-    // Додати атрибут data-modal-close на кнопку закриття
     closeModalBtn: document.querySelector("[data-burger-close]"),
-    // Додати атрибут data-modal на бекдроп модалки
     modal: document.querySelector("[data-burger]"),
   };
 
@@ -12,7 +10,6 @@
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
     refs.modal.classList.toggle("is-open");
   }
 })();
